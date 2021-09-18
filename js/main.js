47b08072b189613d7b30227fad76b02e73b6b3ca
Daria Bichan <dariavorom@gmail.com>
2021-09-18 20:03:56 +0300

    feat: section contacts, fullpage plugin

--- js/main.js
+++ js/main.js
@@ -5,8 +5,16 @@ $(function () {
         nextArrow: '<button class="slider-btn slider-btn__right"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.78161 17L9 9L0.78161 1"/></svg></button>',
         infinite: false,
     })
-$('.question__item-title').on('click', function () {
-    $('.question__item').removeClass('question__item--active');
-    $(this).parent().addClass('question__item--active');
-})
+    $('.question__item-title').on('click', function () {
+        $('.question__item').removeClass('question__item--active');
+        $(this).parent().addClass('question__item--active');
+    })
+    $('#fullpage').fullpage({
+        //options here
+        autoScrolling:true,
+        scrollHorizontally: false,
+        sectionSelector: '.page-section',
+        scrollingSpeed: 700,
+        lazyLoading: true,
+    });
 });
